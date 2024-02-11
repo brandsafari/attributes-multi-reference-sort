95339f4d8e1084426af3b4af01e1f98a46546d1e
mikepecha <mail@mikepecha.com>
2024-02-11 16:07:31 +0200

    sc

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ window.Webflow ||= []
 window.Webflow.push(() => {
    // Sorts a Webflow multi-reference list in ascending order
    // based on an alphanumeric sortvalue element
-   const multiReferenceList = document.querySelector("[brs-multirefsort-element='list']")
-   if (!multiReferenceList) return
-   const listItems = Array.from(multiReferenceList.querySelectorAll(".w-dyn-item"))
+   const multiReferenceLists = Array.from(
+      document.querySelectorAll("[brs-multirefsort-element='list']")
+   )
 
-   listItems.forEach((li) => li.parentElement.removeChild(li))
-   listItems.sort((a, b) => {
-      const sortValueA = a.querySelector("[brs-multirefsort-element='sortvalue']").textContent
-      const sortValueB = b.querySelector("[brs-multirefsort-element='sortvalue']").textContent
-      if (sortValueA > sortValueB) return 1
-      if (sortValueA < sortValueB) return -1
-      return 0
+   multiReferenceLists.forEach((multiReferenceList) => {
+      if (!multiReferenceList) return
+      const listItems = Array.from(multiReferenceList.querySelectorAll(".w-dyn-item"))
+
+      const isAscending =
+         multiReferenceList.getAttribute("brs-multirefsort-order").toLowerCase() !== "desc"
+
+      listItems.forEach((li) => li.parentElement.removeChild(li))
+      listItems.sort((a, b) => {
+         const sortValueA = a.querySelector("[brs-multirefsort-element='sortvalue']").textContent
+         const sortValueB = b.querySelector("[brs-multirefsort-element='sortvalue']").textContent
+         if (sortValueA > sortValueB) return isAscending ? 1 : -1
+         if (sortValueA < sortValueB) return isAscending ? -1 : 1
+         return 0
+      })
+
+      listItems.forEach((li) => multiReferenceList.append(li))
    })
-
-   listItems.forEach((li) => multiReferenceList.append(li))
 })
